fa238109328f5f6dd2b422f7cee431a53d19fde4
Funsho <oluwafunshoalabi97@gmail.com>
2025-12-15 14:17:12 +0100

    hero counting numbers and download

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -13,13 +13,13 @@
             Vue.js, React, JavaScript, Tailwind CSS, and REST APIs. 
             </p>
             <div class="flex">
-                <button @click="handleDownload">
+                <button @click="handleDownload" id="cv-download">
                     Download cv
                     <i><ArrowDown size="20"/></i>
                 </button>
 
                 <div class="social">
-                    <i><Instagram @click="handleIntagram" size="30" /></i>
+                    <i><Instagram @click="handleInstagram" size="30" /></i>
                     <i><Linkedin @click="handleLinkedIn" size="30" /></i>
                 </div>
             </div>
@@ -31,19 +31,23 @@
 
       <div class="experience">
         <div class="flex">
-            <h2>3</h2>
+            <h2 v-if="experienceYears">{{ experienceYears }}</h2>
+            <h2 v-else>0</h2>
             <p>Years of<span>experience</span></p>
         </div>
         <div class="flex">
-            <h2>5</h2>
+            <h2 v-if="projectsCompleted">{{ projectsCompleted }}</h2>
+            <h2 v-else>0</h2>
             <p>Projects<span>completed</span></p>
         </div>
         <div class="flex">
-            <h2>4</h2>
+            <h2 v-if="techMastered">{{ techMastered }}</h2>
+            <h2 v-else>0</h2>
             <p>Technologies<span>mastered</span></p>
         </div>
         <div class="flex">
-            <h2>200</h2>
+            <h2 v-if="commits">{{ commits }}</h2>
+            <h2 v-else>0</h2>
             <p>Code<span>commits</span></p>
         </div>
       </div>
@@ -53,7 +57,7 @@
 
 <script>
 import { ArrowDown, Instagram, Linkedin } from "lucide-vue-next";
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 export default {
     components:{
@@ -63,19 +67,79 @@ export default {
     },
     methods:{
         handleInstagram(){
-            window.location.href = "https://"
+            window.location.href = "https://www.instagram.com/_fun_tun/"
         },
         handleLinkedIn(){
-            window.location.href = "https://"
+            window.location.href = "https://www.linkedin.com/in/funsho-alabi-b55786375/"
         },
-        handleDownload(){
-
-        }
+        
     },
     setup(){
+        const commits = ref(0)
+        const projectsCompleted = ref(0)
+        const experienceYears = ref(0)
+        const techMastered = ref(0)
         onMounted(()=>{
-            console.log("Mounted")
+            // console.log("Mounted")   
+            commits.value = 0
+            projectsCompleted.value = 0
+            experienceYears.value = 0
+            techMastered.value = 0
+            const duration = 3000
+            const max = 200
+            const max1 = 3
+            const max2 = 5
+            const max3 = 9
+            const duration2 = 2000
+            const intervalTime2 = duration2 / max3
+            const intervalTime = duration / max
+
+            const timer = setInterval(() => {
+                commits.value++
+
+                if (commits.value >= max) {
+                    clearInterval(timer);
+                }
+            }, intervalTime);
+
+            const timer2 = setInterval(()=>{
+                techMastered.value++
+
+                if (techMastered.value >= max3) {
+                    clearInterval(timer2);
+                }
+            }, intervalTime2);
+
+            const timer3 = setInterval(()=>{
+                experienceYears.value++
+
+                if (experienceYears.value >= max1) {
+                    clearInterval(timer3);
+                }
+            }, intervalTime2);
+
+            const timer4 = setInterval(()=>{
+                projectsCompleted.value++
+
+                if (projectsCompleted.value >= max2) {
+                    clearInterval(timer4);
+                }
+            }, intervalTime2);
+            
         })
+        
+        function handleDownload(){
+            console.log("download button clciked")
+            const a = document.createElement('a');
+            a.href = "../../files/Oluwafunsho-Alabi-CV.pdf";
+            a.download = "Oluwafunsho-Alabi-CV.pdf";
+            a.click();
+            console.log(a);
+        }
+
+        
+        
+        return{handleDownload, commits, projectsCompleted, experienceYears, techMastered}
     }
 };
 </script>
@@ -128,9 +192,16 @@ export default {
     font-size: 12px;
     font-weight: 600;
     margin-right: 40px;
+    cursor: pointer;
+}
+.hero-about-text button:hover{
+    color: #fff;
 }
 .hero-about-text button i{
     margin-left: 5px;
+}
+.hero-about-text .social i:hover{
+    color: #fff;
 }
 .flex, .experience{
     display: flex;
@@ -172,11 +243,30 @@ export default {
     .hero-about{
         display: block;
     }
+    .hero-about-text p{
+        font-size: 20px;
+        font-family: '';
+    }
+    .experience p{
+        font-size: 20px;
+    }
+    .flex h2{
+        font-size: 70px;
+    }
     .hero-about-photo-1{
         display: block;
         justify-self: center;
         margin-top: 2%;
         margin-bottom: -2%;
+    }
+    .hero-about-photo-1{
+        width: 100%;
+        height: 100%;
+    }
+    .hero-about-photo-1 img{
+        width: calc(100% - 10px);
+        height: calc(100% - 50px);
+        object-fit: cover;
     }
     .hero-about-photo-2{
         display: none;
@@ -196,7 +286,7 @@ export default {
     .experience{
         display: grid;
         grid-template-columns: repeat(2, 1fr);
-        gap: 20px;
+        gap: 10px;
     }
     
 }
